5d14a39e3afb0b88c100fefb07abe49219cf8f06
Toshimitsu Takahashi <toshi@tilfin.com>
2015-08-24 10:58:42 +0900

    Add gc-ddns command, refactor to use CommonUtil

--- resource/record.js
+++ resource/record.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var config = require('config').config;
+var CommonUtil = require('../util/common');
 var record = require('../model/record');
 
 
@@ -25,13 +26,9 @@ exports.post = function(req, res, next) {
   }
 
   var ip = req.params.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  var recordType;
-
-  if (ip.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/)) {
-    recordType = 'A';
-  } else if (ip.match(/^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/)) {
-    recordType = 'AAAA';
-  } else {
+  var ipVersion = CommonUtil.validateIpVersion(ip);
+  var recordType = CommonUtil.getAddressRecordType(ipVersion);
+  if (recordType == null) {
     next(new restify.errors.BadRequestError('Invalid IP address'));
     return;
   }
